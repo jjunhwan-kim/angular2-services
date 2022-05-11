16e009837273f969726f3ec19debbc0f62a68910
JunHwan Kim <nefertiri972@gmail.com>
2022-05-11 15:31:14 +0900

    Inject a service dependency to another service
    - 서비스 클래스에 다른 서비스 의존성을 주입받으려면 주입 받을 서비스 클래스에 @Injectable 데코레이터를 사용해야 함

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,5 +1,8 @@
-export class AccountsService {
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
 
+@Injectable()
+export class AccountsService {
   accounts = [
     {
       name: 'Master Account',
@@ -15,11 +18,15 @@ export class AccountsService {
     }
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
